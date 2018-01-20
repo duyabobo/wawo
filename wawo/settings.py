09fee136980cee8950e719aa6ac0fdd237616125
du <809618694@qq.com>
2018-01-20 19:45:03 +0800

    U settings

--- wawo/settings.py
+++ wawo/settings.py
@@ -75,23 +75,16 @@ WSGI_APPLICATION = 'wawo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wawo',
+        'USER': 'root',
+        'PASSWORD': 'duyabo',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'wawo',
-#         'USER': 'root',
-#         'PASSWORD': 'duyabo',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation
